5bfe72fe4aa37b9957ee162e54066a67eb3c4329
Darwin D Wu <wuddarwin@gmail.com>
2017-10-22 17:07:57 -0700

    update git url

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 # git repository containing pyenv
-default['pyenv']['git_url'] = 'git://github.com/yyuu/pyenv.git'
-default['pyenv']['git_ref'] = 'v0.4.0-20140516'
+default['pyenv']['git_url'] = 'https://github.com/pyenv/pyenv.git'
+default['pyenv']['git_ref'] = 'master'
 
 # upgrade action strategy
 default['pyenv']['upgrade'] = 'none'
