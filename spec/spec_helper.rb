23292e6947cdb77d0afac9f98e1216da3c6deb6e
Darwin D Wu <wuddarwin@gmail.com>
2017-10-23 23:23:40 -0700

    add basic chefspec for default

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
+
+DISTRIBUTIONS = {
+  'ubuntu' => ['14.04', '16.04'],
+  'centos' => ['6.9', '7.3.1611']
+}
+
+at_exit { ChefSpec::Coverage.report! }
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -6,17 +6,18 @@
 
 require 'spec_helper'
 
-describe 'chef-pyenv::default' do
-  context 'When all attributes are default, on an Ubuntu 16.04' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
-      runner.converge(described_recipe)
-    end
+describe 'pyenv::default' do
+  DISTRIBUTIONS.each do |platform, versions|
+    versions.each do |version|
+      cached(:chef_run) do
+        ChefSpec::SoloRunner.new(platform: platform, version: version)
+          .converge(described_recipe)
+      end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+      it 'converges successfully' do
+        # expect(Chef::Recipe).to receive(:include).with(Chef::Pyenv::RecipeHelpers)
+        expect { chef_run }.to_not raise_error
+      end
     end
   end
 end
