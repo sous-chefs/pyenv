fbb2995dfca74d6d0a56a7f50a876fdba0d0196f
Darwin D Wu <wuddarwin@gmail.com>
2017-10-23 22:49:06 -0700

    Add user inspec checks

--- test/smoke/user/controls/user.rb
+++ test/smoke/user/controls/user.rb
@@ -1,9 +1,9 @@
 # encoding: utf-8
 # copyright: 2017, The Authors
 
-control 'system-install-1.0' do
+control 'user-install-1.0' do
   impact 1.0
-  title 'pyenv system installation checks'
+  title 'pyenv user installation checks'
 
   describe file('/etc/profile.d/pyenv.sh') do
     it { should be_file }
@@ -12,32 +12,32 @@ control 'system-install-1.0' do
     its('group')   { should eq('root') }
   end
 
-  describe directory('/usr/local/pyenv') do
+  describe directory('/home/kitchen/.pyenv') do
     it { should exist }
-    its('owner') { should eq('root') }
+    its('owner') { should eq('kitchen') }
     its('group') { should eq('root') }
   end
 
-  describe file('/usr/local/pyenv/bin/pyenv') do
+  describe file('/home/kitchen/.pyenv/bin/pyenv') do
     it { should be_file }
     it { should be_executable }
-    its('owner') { should eq('root') }
+    its('owner') { should eq('kitchen') }
     its('group') { should eq('root') }
   end
 
-  describe file('/usr/local/pyenv/shims/pip') do
+  describe file('/home/kitchen/.pyenv/shims/pip') do
     it { should be_file }
     it { should be_executable }
-    its('owner') { should eq('root') }
+    its('owner') { should eq('kitchen') }
     its('group') { should eq('root') }
   end
 
-  describe command('ls -la /usr/local/pyenv/versions') do
+  describe command('ls -la /home/kitchen/.pyenv/versions') do
     its('stdout') { should match('2.7.6') }
     its('stdout') { should match('3.6.0') }
   end
 
-  describe command('/usr/local/pyenv/shims/python --version') do
+  describe command('/home/kitchen/.pyenv/shims/python --version') do
     its('stderr') { should match('2.7.6') }
   end
 end
