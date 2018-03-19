bc94f229a98b53c0d504e3a2e21d2747870eb94a
Darwin D Wu <wuddarwin@gmail.com>
2018-03-19 13:21:47 -0700

    update version on metadata

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Manages pyenv and its installed Python versions.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url       'https://github.com/darwin67/chef-pyenv/issues'
 source_url       'https://github.com/darwin67/chef-pyenv'
-version          '0.2.0'
+version          '1.0.0'
 chef_version     '>= 12.9' if respond_to?(:chef_version)
 
 %w(
