198ae31798d76e509d091c8d46afd8dff4891a6e
Darwin D Wu <wuddarwin@gmail.com>
2018-03-18 18:16:17 -0700

    add plugin resource and update tests

--- test/fixtures/cookbooks/test/recipes/system_install.rb
+++ test/fixtures/cookbooks/test/recipes/system_install.rb
@@ -5,8 +5,10 @@ version = '3.6.1'
 # Install pyenv globally
 pyenv_system_install 'system'
 
-# Install a Python version
 pyenv_python version
 
-# Set that Python as the global Python
 pyenv_global version
+
+pyenv_plugin 'virtualenv' do
+  git_url 'https://github.com/pyenv/pyenv-virtualenv'
+end
--- test/integration/system_install/controls/system_install.rb
+++ test/integration/system_install/controls/system_install.rb
@@ -6,17 +6,51 @@ control 'pyenv should be installed' do
   title 'pyenv should be installed globally'
 
   desc "Can set global Python versions to #{global_python}"
-  describe bash('source /etc/profile.d/pyenv.sh && pyenv versions') do
+  describe bash('source /etc/profile.d/pyenv.sh && pyenv global') do
     its('exit_status') { should eq(0) }
-    its('stdout')      { should include(global_python) }
+    its('stdout')      { should match(global_python) }
+    its('stdout')      { should_not match('system') }
+  end
+
+  desc "Python #{global_python} should be installed"
+  describe bash('source /etc/profile.d/pyenv.sh && python --version') do
+    its('exit_status') { should eq(0) }
+    its('stdout')      { should match(global_python) }
+  end
+
+  desc 'Plugin should be installed'
+  describe bash('source /etc/profile.d/pyenv.sh && pyenv virtualenv') do
+    its('stderr') { should match('pyenv-virtualenv') }
   end
 end
 
 control 'pyenv should be installed to the system path' do
   title 'pyenv should be installed in the global location'
 
-  describe file('/usr/local/pyenv') do
+  describe file('/etc/profile.d/pyenv.sh') do
+    it { should be_file }
+    it { should be_executable }
+    its('owner') { should eq('root') }
+    its('group') { should eq('root') }
+  end
+
+  describe directory('/usr/local/pyenv') do
     it { should exist }
-    it { should be_directory }
+    its('owner') { should eq('root') }
+    its('group') { should eq('root') }
+  end
+
+  describe file('/usr/local/pyenv/bin/pyenv') do
+    it { should be_file }
+    it { should be_executable }
+    its('owner') { should eq('root') }
+    its('group') { should eq('root') }
+  end
+
+  describe file('/usr/local/pyenv/shims/pip') do
+    it { should be_file }
+    it { should be_executable }
+    its('owner') { should eq('root') }
+    its('group') { should eq('root') }
   end
 end
