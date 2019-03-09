c0dc984eea9bc1e970a4d20dde5d001fd432acae
Darwin D Wu <wuddarwin@gmail.com>
2019-03-08 23:26:31 -0800

    update attributes based on pyenv wiki's recommendation

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,11 +4,12 @@ default['pyenv']['git_ref'] = 'master'
 
 default['pyenv']['prerequisites'] = case node['platform_family']
                                     when 'debian'
-                                      %w(make build-essential libssl-dev zlib1g-dev git-core grep libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm)
+                                      %w(make libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git)
                                     when 'rhel', 'fedora', 'amazon' # oracle, centos, amazon, fedora
-                                      %w(bzip2 bzip2-devel git grep patch readline-devel sqlite sqlite-devel zlib-devel openssl-devel)
+                                      %w(git zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel xz xz-devel libffi-devel findutils)
                                     when 'suse'
-                                      %w(git git-core zlib-devel bzip2 libbz2-devel readline-devel sqlite3 sqlite3-devel libopenssl-devel xz xz-devel)
+                                      # %w(git git-core zlib-devel bzip2 libbz2-devel readline-devel sqlite3 sqlite3-devel libopenssl-devel xz xz-devel)
+                                      %w(git git-core zlib-devel bzip2 libbz2-devel libffi-devel libopenssl-devel readline-devel sqlite3 sqlite3-devel xz xz-devel)
                                     when 'mac_os_x'
-                                      %w(git readline xz)
+                                      %w(git readline xz) # not tested
                                     end
