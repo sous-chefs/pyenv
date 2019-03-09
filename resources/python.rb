6ac729f995296b3a4f79f6aab6c725fefcf5bc90
Darwin D Wu <wuddarwin@gmail.com>
2019-03-08 21:54:32 -0800

    use build-essential resource to install build libraries

--- resources/python.rb
+++ resources/python.rb
@@ -36,6 +36,8 @@ action :install do
 
   Chef::Log.info("Building Python #{new_resource.version}, this could take a while...")
 
+  build_essential 'build packages'
+
   package node['pyenv']['prerequisites']
 
   command = %(pyenv #{new_resource.pyenv_action} #{new_resource.version} #{verbose})
