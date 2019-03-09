97227a45d67865355e468bb959986322b4e74df8
Darwin D Wu <wuddarwin@gmail.com>
2019-03-09 00:02:26 -0800

    update cookbook version

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Manages pyenv and its installed Python versions.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url       'https://github.com/darwin67/chef-pyenv/issues'
 source_url       'https://github.com/darwin67/chef-pyenv'
-version          '2.1.0'
+version          '3.0.0'
 chef_version     '>= 14.0' if respond_to?(:chef_version)
 
 %w(
