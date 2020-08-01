e6956c3ea3904049ef49c339c7c9172aabb5b992
MarkGibbons <gibbonsmj80@gmail.com>
2020-08-01 14:01:11 -0700

    Test centos, debian and ubuntu versions

--- test/integration/user_install/controls/user_install_spec.rb
+++ test/integration/user_install/controls/user_install_spec.rb
@@ -1,80 +1,78 @@
 # frozen_string_literal: true
 
-global_python = '3.7.7'
-venv_root = '/opt/venv_test'
+global_python = '3.6.1'
+user          = 'vagrant'
+venv_root     = "/home/#{user}/venv_test"
 
 control 'pyenv should be installed' do
-  title 'pyenv should be installed globally'
+  title 'pyenv should be installed to the users home directory'
 
   desc "Can set global Python versions to #{global_python}"
-  describe bash('source /etc/profile.d/pyenv.sh && pyenv global') do
+  describe bash("sudo -H -u #{user} bash -c 'source /etc/profile.d/pyenv.sh && pyenv global'") do
     its('exit_status') { should eq(0) }
-    its('stdout')      { should match(global_python) }
-    its('stdout')      { should_not match('system') }
+    its('stdout') { should match(global_python) }
+    its('stdout') { should_not match('system') }
   end
 
   desc "Python #{global_python} should be installed"
-  describe bash('source /etc/profile.d/pyenv.sh && python --version') do
+  describe bash("sudo -H -u #{user} bash -c 'source /etc/profile.d/pyenv.sh && python --version'") do
     its('exit_status') { should eq(0) }
     its('stdout')      { should match(global_python) }
   end
 
   desc 'Plugin should be installed'
-  describe bash('source /etc/profile.d/pyenv.sh && pyenv virtualenv') do
+  describe bash("sudo -H -u #{user} bash -c 'source /etc/profile.d/pyenv.sh && pyenv virtualenv'") do
     its('stderr') { should match('pyenv-virtualenv') }
   end
 
   desc 'Pip should install package requests'
-  describe bash("sudo -H bash -c 'source /etc/profile.d/pyenv.sh && pip show requests'") do
+  describe bash("sudo -H -u #{user} bash -c 'source /etc/profile.d/pyenv.sh && pip show requests'") do
     its('exit_status') { should eq(0) }
     its('stdout')      { should match('Version: 2.18.3') }
   end
 
   desc 'Pip should install package virtualenv'
-  describe bash("sudo -H bash -c 'source /etc/profile.d/pyenv.sh && pip show virtualenv'") do
+  describe bash("sudo -H -u #{user} bash -c 'source /etc/profile.d/pyenv.sh && pip show virtualenv'") do
     its('exit_status') { should eq(0) }
     its('stdout')      { should match('Version: 16.2.0') }
   end
 
   desc 'Pip should install package requests inside virtualenv according to requirements.txt'
-  describe bash("sudo -H bash -c 'source /etc/profile.d/pyenv.sh && #{venv_root}/bin/pip show fire'") do
+  describe bash("sudo -H -u #{user} bash -c 'source /etc/profile.d/pyenv.sh && #{venv_root}/bin/pip show fire'") do
     its('exit_status') { should eq(0) }
     its('stdout')      { should match('Version: 0.1.2') }
   end
 
   desc 'Pip should uninstall package requests inside virtualenv'
-  describe bash("sudo -H bash -c 'source /etc/profile.d/pyenv.sh && #{venv_root}/bin/pip show requests'") do
+  describe bash("sudo -H -u #{user} bash -c 'source /etc/profile.d/pyenv.sh && #{venv_root}/bin/pip show requests'") do
     its('exit_status') { should eq(1) }
   end
 end
 
-control 'pyenv should be installed to the system path' do
-  title 'pyenv should be installed in the global location'
+control 'pyenv should be installed to the user path' do
+  title "pyenv should be installed in the user's home"
 
   describe file('/etc/profile.d/pyenv.sh') do
     it { should be_file }
     it { should be_executable }
     its('owner') { should eq('root') }
-    its('content') { should match(/^\s+pyenv_init="pyenv init -"/) }
-    its('content') { should match(/Rehashing will fail in a system install\n\s+pyenv_init="pyenv init - --no-rehash"/m) }
-    its('content') { should match(/^\s+eval "\$\(\$pyenv_init\)"/) }
   end
 
-  describe directory('/usr/local/pyenv') do
+  describe directory("/home/#{user}/.pyenv") do
     it { should exist }
-    its('owner') { should eq('root') }
+    its('owner') { should eq(user) }
   end
 
-  describe file('/usr/local/pyenv/bin/pyenv') do
+  describe file("/home/#{user}/.pyenv/bin/pyenv") do
     it { should be_file }
     it { should be_executable }
-    its('owner') { should eq('root') }
+    its('owner') { should eq(user) }
   end
 
-  describe file('/usr/local/pyenv/shims/pip') do
+  describe file("/home/#{user}/.pyenv/shims/pip") do
     it { should be_file }
     it { should be_executable }
-    its('owner') { should eq('root') }
+    its('owner') { should eq(user) }
   end
 end
 
@@ -83,11 +81,11 @@ control 'virtualenv should be created' do
 
   describe directory(venv_root) do
     it { should exist }
-    its('owner') { should eq('root') }
+    its('owner') { should eq(user) }
   end
 
   describe file("#{venv_root}/bin/activate") do
     it { should be_file }
-    its('owner') { should eq('root') }
+    its('owner') { should eq(user) }
   end
 end
