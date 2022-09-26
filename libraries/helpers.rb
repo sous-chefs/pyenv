2257e07d1ea4f41c9805ae4a34583569e1e58bea
Giacomo Bagnoli <gbagnoli@gmail.com>
2022-09-26 22:08:09 +0100

    lint

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -54,14 +54,14 @@ class PyEnv
         value_for_platform_family(
         'debian' =>  %w(make libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev git) \
           << value_for_platform(
-            'debian' => { '>= 11' => 'python3-openssl' , 'default' => 'python-openssl' },
+            'debian' => { '>= 11' => 'python3-openssl', 'default' => 'python-openssl' },
             'ubuntu' => { '>= 22.04' => 'python3-openssl', 'default' => 'python-openssl' },
             'default' => 'python-openssl'
           ),
-        ['rhel', 'fedora', 'amazon'] =>  %w(git zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel xz xz-devel libffi-devel findutils),
+        %w(rhel fedora amazon) =>  %w(git zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel xz xz-devel libffi-devel findutils),
         'suse' => %w(git git-core zlib-devel bzip2 libbz2-devel libopenssl-devel readline-devel sqlite3 sqlite3-devel xz xz-devel),
-        'mac_os_x' => %w(git readline xz),
-        )
+        'mac_os_x' => %w(git readline xz)
+      )
       end
     end
   end
